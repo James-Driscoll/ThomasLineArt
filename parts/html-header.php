06d12a04263c8870385e5bd8a999ca325a02413c
James Driscoll <james.e.n.driscoll>
2014-08-11 22:21:44 +0100

    Development to header section.

--- parts/html-header.php
+++ parts/html-header.php
@@ -3,9 +3,9 @@
 <!--[if lt IE 7 ]><html class="no-js ie6" lang="en"><![endif]-->
 <!--[if IE 7 ]><html class="no-js ie7" lang="en"><![endif]-->
 <!--[if IE 8 ]><html class="no-js ie8" lang="en"><![endif]-->
-<!--[if (gte IE 9)|(gt IEMobile 7)|!(IEMobile)|!(IE)]><!--><html class="no-js" lang="en"><!--<![endif]-->
-	<head>
-            
+<!--[if (gte IE 9)|(gt IEMobile 7)|!(IEMobile)|!(IE)]><!-->
+<html class="no-js" lang="en"><!--<![endif]-->
+	<head>   
         <!-- Load up Google Fonts FAST -->
         <link href='http://fonts.googleapis.com/css?family=Neuton:300,400,700' rel='stylesheet' type='text/css'>
         <link href='http://fonts.googleapis.com/css?family=Lato:400,900' rel='stylesheet' type='text/css'>
@@ -49,14 +49,16 @@
     <body <?php body_class(); ?>>
 
         <div class="container header">
+            
             <h1 class="title"><a href="http://art.thomasline.uk">Thomas Line</a></h1>
-        </div>
+        
+			<?php
+				//Output the navigation
+			  	$args = array(
+				  "theme_location" => "top_navigation",
+				  'container_class' => 'top-navigation container'
+				);
+				wp_nav_menu($args);
+			?>
 
-		<?php
-			//Output the navigation
-		  	$args = array(
-			  "theme_location" => "top_navigation",
-			  'container_class' => 'top-navigation container'
-			);
-			wp_nav_menu($args);
-		?>
+		</div>

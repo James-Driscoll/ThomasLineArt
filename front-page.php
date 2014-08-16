9975cdc5ac51af38c22865e6cee92972ddcfea80
James <James@ULTRANOVA>
2014-08-16 15:36:58 +0100

    Updated front-page.php file.

--- front-page.php
+++ front-page.php
@@ -1,15 +1,15 @@
 <?php
-/**
- * Template Name: Homepage
- *
- * @package     WordPress
- * @subpackage  Starkers
- * @since       Starkers 4.0
- *
- * Please see /external/starkers-utilities.php for info on get_template_parts()
- */
+  /**
+   * Template Name: Homepage
+   *
+   * @package     WordPress
+   * @subpackage  Starkers
+   * @since       Starkers 4.0
+   *
+   * Please see /external/starkers-utilities.php for info on get_template_parts()
+   */
 
-//if ( have_posts() ) while ( have_posts() ) : the_post();
+  //if ( have_posts() ) while ( have_posts() ) : the_post();
  ?>
 
 <!DOCTYPE HTML>
@@ -18,64 +18,66 @@
 <!--[if IE 7 ]><html class="no-js ie7" lang="en"><![endif]-->
 <!--[if IE 8 ]><html class="no-js ie8" lang="en"><![endif]-->
 <!--[if (gte IE 9)|(gt IEMobile 7)|!(IEMobile)|!(IE)]><!--><html class="no-js" lang="en"><!--<![endif]-->
-    <head>
-            
-        <!-- Load up Google Fonts FAST -->
-        <link href='http://fonts.googleapis.com/css?family=Neuton:300,400,700' rel='stylesheet' type='text/css'>
-        <link href='http://fonts.googleapis.com/css?family=Lato:400,900' rel='stylesheet' type='text/css'>
-        <link href='http://fonts.googleapis.com/css?family=Shadows+Into+Light+Two' rel='stylesheet' type='text/css'>
-        
-        <title><?php wp_title( '|' ); ?></title>
-        <meta charset="<?php bloginfo( 'charset' ); ?>" />
-        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 
-        <link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri(); ?>/favicon.ico"/>
-        <link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri(); ?>/style.css?v=3"/>
-        <!--<link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri(); ?>/normalize.css?v=3"/>-->
+<head>        
+    <!-- Load up Google Fonts FAST -->
+    <link href='http://fonts.googleapis.com/css?family=Neuton:300,400,700' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Lato:400,900' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Shadows+Into+Light+Two' rel='stylesheet' type='text/css'>
 
-        <meta name="viewport" content="width=device-width">
+    <title><?php wp_title( '|' ); ?></title>
 
-        <!-- DNS Prefetch -->
-        <link rel="dns-prefetch" href="<?php echo get_bloginfo("url"); ?>" />
-        <link rel="dns-prefetch" href="//ajax.googleapis.com" />
-        <link rel="dns-prefetch" href="//maps.google.com" />
+    <meta charset="<?php bloginfo( 'charset' ); ?>" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 
-        <!-- IE Stuff -->
-        <meta http-equiv="imagetoolbar" content="false" />
+    <link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri(); ?>/favicon.ico"/>
+    <link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri(); ?>/style.css?v=3"/>
 
-        <!-- JQuery -->
-        <script src="<?php echo get_stylesheet_directory_uri(); ?>/js/jquery-1.7.1.min.js" ></script>
+    <!--<link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri(); ?>/normalize.css?v=3"/>-->
+    <meta name="viewport" content="width=device-width">
 
-        <!-- BxSlider -->
-        <script src="<?php echo get_stylesheet_directory_uri();?>/js/jquery.bxSlider.min.js"></script>
+    <!-- DNS Prefetch -->
+    <link rel="dns-prefetch" href="<?php echo get_bloginfo("url"); ?>" />
+    <link rel="dns-prefetch" href="//ajax.googleapis.com" />
+    <link rel="dns-prefetch" href="//maps.google.com" />
 
-        <!-- Site Javascript -->
-        <script src="<?php echo get_stylesheet_directory_uri();?>/js/site.js"></script>
+    <!-- IE Stuff -->
+    <meta http-equiv="imagetoolbar" content="false" />
 
+    <!-- JQuery -->
+    <script src="<?php echo get_stylesheet_directory_uri(); ?>/js/jquery-1.7.1.min.js" ></script>
 
+    <!-- BxSlider -->
+    <script src="<?php echo get_stylesheet_directory_uri();?>/js/jquery.bxSlider.min.js"></script>
 
-<?php wp_head(); ?>
+    <!-- Site Javascript -->
+    <script src="<?php echo get_stylesheet_directory_uri();?>/js/site.js"></script>
 
-        <script>site = {ss: "<?php echo get_stylesheet_directory_uri(); ?>"};</script>
-        </head>
-        <body <?php body_class(); ?>>
-
-
-<!-- Content Begins Here -->
-
-<div class="home-nav">
     <?php
-        //Output the navigation
-        $args = array(
+      wp_head();
+    ?>
+
+    <script>site = {ss: "<?php echo get_stylesheet_directory_uri(); ?>"};</script>
+
+</head>
+
+<body <?php body_class(); ?>>
+
+    <!-- Content Begins Here -->
+
+    <div class="home-nav">
+        <?php
+          //Output the navigation
+          $args = array(
             "theme_location" => "top_navigation",
             'container_class' => 'top-navigation container'
-        );
-        wp_nav_menu($args);
-    ?>
-</div>
+          );
+          wp_nav_menu($args);
+        ?>
+    </div>
 
-<a class="home-logo" href="<?php echo get_site_url(); ?>">
-    <img class="logo" height="576" width="720" src="<?php echo get_stylesheet_directory_uri() ?>/images/logo-home.gif" alt="Thomas Line Art logo">
-</a>
+    <a class="home-logo" href="<?php echo get_site_url(); ?>">
+        <img class="logo" height="576" width="720" src="<?php echo get_stylesheet_directory_uri() ?>/images/logo-home.gif" alt="Thomas Line Art logo">
+    </a>
 
-<!-- Content ends here -->
+    <!-- Content ends here -->

97b7e92ce8543cb912b37dc0ade6f0fa60d8c22f
James <James@ULTRANOVA>
2014-09-08 15:30:43 +0100

    Moved page.php out of the Parts folder.

--- single-jdtla_studio.php
+++ single-jdtla_studio.php
@@ -2,9 +2,6 @@
 /**
  * Template Name: Studio & Research
  * 
- * @package     WordPress
- * @subpackage  Starkers
- * @since       Starkers 4.0
  *
  */
 
@@ -15,6 +12,7 @@ get_template_parts( array( 'parts/html-header') ); ?>
 $query = new WP_Query( array('post_type' => 'jdtla_studio', 'posts_per_page' => 5 ) );
 	while ( $query->have_posts() ) : $query->the_post(); ?>
 
+
 <?php
 $meta = rwmb_meta("studio_images");
 $image = array("none.jpg");
@@ -22,10 +20,10 @@ if ($meta) {
     $image = wp_get_attachment_image_src($meta,"post-medium");
 } ?>
 
-<ul class="studio-item">
-	<li class="studio-image"><a href="#"><img src="<?php echo $image[0]; ?>" height="230" width="230" /></a></li>
-	<li class="studio-title"><h2><?php the_title(); ?></h2></li>
-</ul>
+<a class="container studio-item" href="#">
+	<img class="studio-image" src="<?php echo $image[0]; ?>" height="230" width="230">
+	<h2 class="studio-title"><?php the_title(); ?></h2>
+</a/
 
 <?php  wp_reset_postdata(); ?>
 <?php endwhile;

3798a0c5d3b5ae0d044f9ce6509a7f6d3877cbd0
James <James@ULTRANOVA>
2014-09-11 20:59:56 +0100

    Added target="_blank" to the footer links.

--- parts/html-footer.php
+++ parts/html-footer.php
@@ -1,7 +1,7 @@
 <!-- Footer begins here -->
 <div class="container footer">
-    <div class="email"><a href="mailto:enquiries@thomasline.uk">enquiries@thomasline.uk</a></div>
-    <div class="developer"><a href="http://jamesdriscoll.london">Wordpress theme developed by James Driscoll</a></div>
+    <div class="email"><a href="mailto:enquiries@thomasline.uk" target="_blank">enquiries@thomasline.uk</a></div>
+    <div class="developer"><a href="http://jamesdriscoll.london" target="_blank">Wordpress theme developed by James Driscoll</a></div>
     <div class="copy">Copyright Thomas Line</div>
         
 </div>

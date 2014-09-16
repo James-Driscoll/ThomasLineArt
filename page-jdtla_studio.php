c9c0885a3dc8e0502f3c659e0215b3f48f87be98
James <James@ULTRANOVA>
2014-09-16 19:41:26 +0100

    Removed unused home logo. Reduced margin of studio-item elements.

--- page-jdtla_studio.php
+++ page-jdtla_studio.php
@@ -12,21 +12,12 @@ get_template_parts( array( 'parts/html-header') ); ?>
 <div class="studio">
 	<?php $query = new WP_Query( array('post_type' => 'jdtla_studio', 'posts_per_page' => 1000 ) );
 	
-	while ( $query->have_posts() ) : $query->the_post();
+	while ( $query->have_posts() ) : $query->the_post(); ?>
 
-		$meta = rwmb_meta("studio_images");
-		$image = array("none.jpg");
-		if ($meta) {
-		    $image = wp_get_attachment_image_src($meta,"post-medium");
-		} ?>
-
-		
-			<a class="studio-item" href="<?php the_permalink() ?>">
-				
-				<?php the_post_thumbnail('medium'); ?>
-				<h2 class="studio-title"><?php the_title(); ?></h2>
-			</a>
-		
+		<a class="studio-item" href="<?php the_permalink() ?>">
+			<?php the_post_thumbnail('medium'); ?>
+			<h2 class="studio-title"><?php the_title(); ?></h2>
+		</a>
 
 		<?php  wp_reset_postdata();
 

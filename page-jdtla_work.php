3c8ef48d2b43fa4b387a778f293a546dfb2501b6
James <James@ULTRANOVA>
2014-10-25 20:30:12 +0100

    Added front-end code to output each custom category for the Work post type.

--- page-jdtla_work.php
+++ page-jdtla_work.php
@@ -14,18 +14,70 @@ get_template_parts( array( 'parts/html-header') ); ?>
 <div class="container">
 	<div class="page-studio">
 
-	<div class=""
+		<div class="pc pc-1">
+			<h1>Video</h1>
+			<?php $query = new WP_Query( array('post_type' => 'jdtla_work', 'posts_per_page' => 1000 ) );
+			while ( $query->have_posts() ) : $query->the_post(); ?>
 
-		<?php $query = new WP_Query( array('post_type' => 'jdtla_work', 'posts_per_page' => 1000 ) );
-		while ( $query->have_posts() ) : $query->the_post(); ?>
+				<?php if (pa_in_taxonomy("categories", "video")) { ?>
+					<a class="work-item" href="<?php the_permalink() ?>">
+						<?php the_post_thumbnail('medium'); ?>
+					</a>
+				<?php } ?>
 
-			<a class="work-item" href="<?php the_permalink() ?>">
-				<?php the_post_thumbnail('medium'); ?>
-			</a>
+				<?php  wp_reset_postdata();
 
-			<?php  wp_reset_postdata();
+			endwhile; ?>
+		</div>
 
-		endwhile; ?>
+		<div class="pc pc-2">
+			<h1>2-D</h1>
+			<?php $query = new WP_Query( array('post_type' => 'jdtla_work', 'posts_per_page' => 1000 ) );
+			while ( $query->have_posts() ) : $query->the_post(); ?>
+
+				<?php if (pa_in_taxonomy("categories", "2-D")) { ?>
+					<a class="work-item" href="<?php the_permalink() ?>">
+						<?php the_post_thumbnail('medium'); ?>
+					</a>
+				<?php } ?>
+
+				<?php  wp_reset_postdata();
+
+			endwhile; ?>
+		</div>
+
+		<div class="pc pc-3">
+			<h1>3-D</h1>
+			<?php $query = new WP_Query( array('post_type' => 'jdtla_work', 'posts_per_page' => 1000 ) );
+			while ( $query->have_posts() ) : $query->the_post(); ?>
+
+				<?php if (pa_in_taxonomy("categories", "3-D")) { ?>
+					<a class="work-item" href="<?php the_permalink() ?>">
+						<?php the_post_thumbnail('medium'); ?>
+					</a>
+				<?php } ?>
+
+				<?php  wp_reset_postdata();
+
+			endwhile; ?>
+		</div>
+
+		<div class="pc pc-4">
+			<h1>Instalation</h1>
+			<?php $query = new WP_Query( array('post_type' => 'jdtla_work', 'posts_per_page' => 1000 ) );
+			while ( $query->have_posts() ) : $query->the_post(); ?>
+
+				<?php if (pa_in_taxonomy("categories", "installation")) { ?>
+					<a class="work-item" href="<?php the_permalink() ?>">
+						<?php the_post_thumbnail('medium'); ?>
+					</a>
+				<?php } ?>
+
+				<?php  wp_reset_postdata();
+
+			endwhile; ?>
+		</div>
+
 	</div>
 </div>
 

6a3f00fe2a4019d37edb96a6e5c29e8003e341bd
James <James@ULTRANOVA>
2014-10-27 13:04:04 +0000

    Added IF statement to remove Work category title if no posts exist of that category.

--- page-jdtla_work.php
+++ page-jdtla_work.php
@@ -15,67 +15,71 @@ get_template_parts( array( 'parts/html-header') ); ?>
 	<div class="page-studio">
 
 		<div class="pc pc-1">
-			<h1>Video</h1>
+			<?php $title = False; ?>
 			<?php $query = new WP_Query( array('post_type' => 'jdtla_work', 'posts_per_page' => 1000 ) );
-			while ( $query->have_posts() ) : $query->the_post(); ?>
-
-				<?php if (pa_in_taxonomy("categories", "video")) { ?>
+			while ( $query->have_posts() ) : $query->the_post();
+				if (pa_in_taxonomy("categories", "video")) {
+					if ($title == False) { ?>
+						<h1 class="pc-title">Video</h1>
+						<?php $title = True;
+					} ?>
 					<a class="work-item" href="<?php the_permalink() ?>">
 						<?php the_post_thumbnail('medium'); ?>
 					</a>
 				<?php } ?>
-
-				<?php  wp_reset_postdata();
-
-			endwhile; ?>
+				<?php  wp_reset_postdata(); ?>
+			<?php endwhile; ?>
 		</div>
 
 		<div class="pc pc-2">
-			<h1>2-D</h1>
+			<?php $title = False; ?>
 			<?php $query = new WP_Query( array('post_type' => 'jdtla_work', 'posts_per_page' => 1000 ) );
-			while ( $query->have_posts() ) : $query->the_post(); ?>
-
-				<?php if (pa_in_taxonomy("categories", "2-D")) { ?>
+			while ( $query->have_posts() ) : $query->the_post();
+				if (pa_in_taxonomy("categories", "2-D")) {
+					if ($title == False) { ?>
+						<h1 class="pc-title">2-D</h1>
+						<?php $title = True;
+					} ?>
 					<a class="work-item" href="<?php the_permalink() ?>">
 						<?php the_post_thumbnail('medium'); ?>
 					</a>
 				<?php } ?>
-
-				<?php  wp_reset_postdata();
-
-			endwhile; ?>
+				<?php  wp_reset_postdata(); ?>
+			<?php endwhile; ?>
 		</div>
 
 		<div class="pc pc-3">
-			<h1>3-D</h1>
+			<?php $title = False; ?>
 			<?php $query = new WP_Query( array('post_type' => 'jdtla_work', 'posts_per_page' => 1000 ) );
-			while ( $query->have_posts() ) : $query->the_post(); ?>
-
-				<?php if (pa_in_taxonomy("categories", "3-D")) { ?>
+			while ( $query->have_posts() ) : $query->the_post();
+				if (pa_in_taxonomy("categories", "3-D")) {
+					if ($title == False) { ?>
+						<h1 class="pc-title">3-D</h1>
+						<?php $title = True;
+					} ?>
 					<a class="work-item" href="<?php the_permalink() ?>">
 						<?php the_post_thumbnail('medium'); ?>
 					</a>
 				<?php } ?>
-
-				<?php  wp_reset_postdata();
-
-			endwhile; ?>
+				<?php  wp_reset_postdata(); ?>
+			<?php endwhile; ?>
 		</div>
 
 		<div class="pc pc-4">
-			<h1>Instalation</h1>
+			<?php $title = False; ?>
 			<?php $query = new WP_Query( array('post_type' => 'jdtla_work', 'posts_per_page' => 1000 ) );
-			while ( $query->have_posts() ) : $query->the_post(); ?>
-
-				<?php if (pa_in_taxonomy("categories", "installation")) { ?>
+			while ( $query->have_posts() ) : $query->the_post();
+				if (pa_in_taxonomy("categories", "installation")) {
+					if ($title == False) { ?>
+						<h1 class="pc-title">Installation</h1>
+						<?php $title = True;
+					} ?>
 					<a class="work-item" href="<?php the_permalink() ?>">
 						<?php the_post_thumbnail('medium'); ?>
 					</a>
 				<?php } ?>
-
-				<?php  wp_reset_postdata();
-
-			endwhile; ?>
+				<?php  wp_reset_postdata(); ?>
+			<?php endwhile; ?>
 		</div>
 
 	</div>

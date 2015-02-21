df4bede0ebbc2e7d15b9aaa5697c8346e1ad1742
james-driscoll <james.e.m.driscoll@gmail.com>
2015-02-21 00:27:44 +0000

    Added basic styling to Work categories list.

--- page-jdtla_work.php
+++ page-jdtla_work.php
@@ -11,30 +11,22 @@
 //Get The Header
 get_template_parts( array( 'parts/html-header') ); ?>
 
-<div class="container">
-	<div class="page-studio">
+<div class="page-work container">
 		 
 	<?php
 	$taxonomy = 'jdtla_work_categories';
 	$terms = get_terms($taxonomy); // Get all terms of a taxonomy
 
-	foreach ( $terms as $term ) { ?>
-			
-		Category: <?php echo $term->name; ?>	  
-		<?php
-		  query_posts( array( 'post_type' => 'jdtla_work', $taxonomy => $term->name) );
-		  //the loop start here
-		  if ( have_posts() ) : while ( have_posts() ) : the_post();
-		?>
-		  <h3><?php the_title(); ?></h3>
-		  <?php the_post_thumbnail('medium'); ?>
-		<?php endwhile; endif; wp_reset_query(); ?>
+	foreach ( $terms as $term ) { ?>	
+		<h1 class="pc-title"><?php echo $term->name; ?></h1>
+		<?php query_posts( array( 'post_type' => 'jdtla_work', $taxonomy => $term->name) );
+	  	if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+	  		<a class="item" href="<?php the_permalink() ?>">
+				<?php the_post_thumbnail('medium'); ?>
+			</a>
+		<?php endwhile; endif; wp_reset_query();
+	} ?>
 
-
-	<?php } ?>
-
-
-	</div>
 </div>
 
 <?php //Get The Footer

a91d8754762d39d6ebaceb22b060804c71c9830c
james-driscoll <james.e.m.driscoll@gmail.com>
2015-12-06 15:24:52 +0000

    Fixed position for left side.

--- front-page.php
+++ front-page.php
@@ -17,13 +17,15 @@
     get_template_parts( array( 'parts/html-header') );
     ?>
 
-    <div class="left-side side all">
-      <div class="logoWrapper">
+    <div class="left-side">
+      
+      <div class="logo">
         <a href="<?php echo get_site_url(); ?>">
-          <img class="logo "height="152" width="200" src="<?php echo get_stylesheet_directory_uri() ?>/images/logo-medium.png" alt="Thomas Line Art">
+          <img height="" width="140" src="<?php echo get_stylesheet_directory_uri() ?>/images/logo-smaller.png" alt="Thomas Line Art">
         </a>
       </div>
-      <div class="nav">
+
+      <div class="navWrapper">
         <?php wp_nav_menu (array('theme_location' => 'page_navigation','container_class' => 'nav')); ?>
       </div>
 
@@ -37,10 +39,12 @@
 
     </div>
 
-    <div class="centre all">
+
+
+    <div class="right-side">
       <div class="videoWrapper">
         <a href="https://vimeo.com/thomasline" target="_blank">
-          <iframe src="https://player.vimeo.com/video/137009360?autoplay=1&loop=1" width="100%" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
+          <iframe src="https://player.vimeo.com/video/146631709?title=0&byline=0&portrait=0" width="700" height="394" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
         </a>
       </div>
 
@@ -63,13 +67,8 @@ Section 1.10.33 of "de Finibus Bonorum et Malorum", written by Cicero in 45 BC
 1914 translation by H. Rackham
 
 "On the other hand, we denounce with righteous indignation and dislike men who are so beguiled and demoralized by the charms of pleasure of the moment, so blinded by desire, that they cannot foresee the pain and trouble that are bound to ensue; and equal blame belongs to those who fail in their duty through weakness of will, which is the same as saying through shrinking from toil and pain. These cases are perfectly simple and easy to distinguish. In a free hour, when our power of choice is untrammelled and when nothing prevents our being able to do what we like best, every pleasure is to be welcomed and every pain avoided. But in certain circumstances and owing to the claims of duty or the obligations of business it will frequently occur that pleasures have to be repudiated and annoyances accepted. The wise man therefore always holds in these matters to this principle of selection: he rejects pleasures to secure other greater pleasures, or else he endures pains to avoid worse pains."</P>
-<!--
-    </div>
 
-    <div class="right-side side all">
 
-    </div>
--->
 <?php
 //Get the footer
 get_template_parts( array( 'parts/html-footer') );

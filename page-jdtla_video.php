f4402c496739927adc09d081c4f02b4449ca523f
James Driscoll <james.e.m.driscoll@gmail.com>
2016-01-28 22:51:19 +0000

    Styled Video Post

--- page-jdtla_video.php
+++ page-jdtla_video.php
@@ -14,15 +14,19 @@ get_template_parts( array( 'parts/html-header') ); ?>
 
 <div class="centre all">
 	<div class="container">
-		<div class="cp-page">
+		<div class="video-page">
 			<?php $query = new WP_Query( array('post_type' => 'jdtla_video', 'posts_per_page' => 1000 ) );
 			while ( $query->have_posts() ) : $query->the_post(); ?>
-				<iframe src="<?php the_field('video_embed'); ?>" width="800" height="450" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
-				<p><?php the_field('camera'); ?></p>
-				<p><?php the_field('duration'); ?></p>
-				<p><?php the_content(); ?></p>
-				<?php  wp_reset_postdata();
-			endwhile; ?>
+				<div class="video">
+					<iframe class="feature" src="<?php the_field('video_embed'); ?>" width="100%" height="600" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
+					<div class="video_details">
+						<strong class="camera"><?php the_field('camera'); ?></strong>, <span class="video_duration"><?php the_field('duration'); ?></span>
+						<h3 class="synopsis-title">Synopsis -</h3>
+						<p class="synopsis-text"><?php the_content(); ?></p>
+					</div>
+					<?php  wp_reset_postdata(); ?>
+				</div>
+			<?php endwhile; ?>
 		</div>
 	</div>
 </div>

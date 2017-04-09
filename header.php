6a478339b43fa92b638d4f7758e5e0926d93766b
James Driscoll <james.e.m.driscoll@gmail.com>
2017-04-09 19:32:48 +0100

    Added LinkedIn icon to social bar.

--- header.php
+++ header.php
@@ -65,9 +65,10 @@
 
 				<div class="social">
 					<ul>
-						<li><a href="https://instagram.com/tomline92/" target="_blank"><i class="fa fa-instagram"></i></a></li>
-						<li><a href="https://twitter.com/TomLine3" target="_blank"><i class="fa fa-twitter"></i></a></li>
 						<li><a href="https://vimeo.com/thomasline" target="_blank"><i class="fa fa-vimeo"></i></a></li>
+						<li><a href="https://www.linkedin.com/in/thomas-line-073933118/" target="_blank"><i class="fa fa-linkedin"></i></a></li>
+						<li><a href="https://twitter.com/TomLine3" target="_blank"><i class="fa fa-twitter"></i></a></li>
+						<li><a href="https://instagram.com/tomline92/" target="_blank"><i class="fa fa-instagram"></i></a></li>
 						<li><a href="https://www.facebook.com/ThomasLineFilmmaker" target="_blank"><i class="fa fa-facebook"></i></a></li>
 					</ul>
 				</div>
